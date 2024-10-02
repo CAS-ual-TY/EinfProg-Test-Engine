6214980cc8c6df5d483dc80a8329ee82f107e4d1
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2024-10-02 10:50:49 +0200

    Bsp00

---
+++ assignment/src/einfprog/Bsp00.java
@@ -0,0 +1,17 @@
+// Vorname Nachname, Matrikelnummer
+package einfprog;
+
+/*
+ * Das einfachste moegliche Java-Programm.
+ */
+
+// Dateiname und Klassenname muessen gleich sein.
+public class Bsp00 {
+    
+    // Hier beginnt die Ausfuehrung des Programms.
+    public static void main(String[] args) {
+        
+        // So wird eine Textzeile auf den Bildschirm gezaubert.
+        System.out.println("Hello World");
+    }
+}
---
+++ test/einfprog/Bsp00Test.java
@@ -0,0 +1,15 @@
+package einfprog;
+
+import einfprog.test_engine.TestMaker;
+import einfprog.test_engine.output.Compound;
+import org.junit.jupiter.api.Test;
+
+public class Bsp00Test
+{
+    @Test
+    public void test1() {
+        TestMaker.callMain("einfprog.Bsp00")
+                .expectConsoleOutput(Compound.construct("Hello World"))
+                .runTest();
+    }
+}
